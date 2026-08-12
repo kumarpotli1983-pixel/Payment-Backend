2b3bb45436321a6694c9a80f8613319bcb42da20
kumarpotli1983-pixel <kumarpotli1983@gmail.com>
2026-08-12 22:20:38 +0530

    fix: add user account existence check in createAccount function

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -1,13 +1,22 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Account } from "../models/account.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
 
 const createAccount = asyncHandler(async(req,res)=>{
 
   const user = req?.user
 
+  const userAccountExists = await Account.findOne({user:req.user._id})
+
+  if(userAccountExists)
+  {
+    throw new ApiError(409,"User Account exists already")
+  }
+
   const account = await Account.create({
-    user:user._id
+    user:user._id,
+    username:req.user.name
   })
 
   res.status(201)
